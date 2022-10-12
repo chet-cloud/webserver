c2a04d742dc42a648690f7139e8836869a124bd3
cc <cchen@artisreit.com>
2022-10-11 23:28:10 -0500

    add example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod db;
 pub mod pool;
+pub mod cc;
 
 use std::io::Error;
 use std::thread::JoinHandle;
@@ -71,6 +72,37 @@ fn handle_connection(mut stream: TcpStream) {
 
 #[cfg(test)]
 mod test {
+    extern crate r2d2;
+    extern crate r2d2_sqlite;
+    extern crate rusqlite;
+    
+    use std::thread;
+    use r2d2_sqlite::SqliteConnectionManager;
+    use rusqlite::params;
 
+    #[test]
+    fn test() {
+        let manager = SqliteConnectionManager::file("file.db");
+        let pool = r2d2::Pool::new(manager).unwrap();
+        pool.get()
+            .unwrap()
+            .execute("CREATE TABLE IF NOT EXISTS foo (bar INTEGER)", params![])
+            .unwrap();
+    
+        (0..1000)
+            .map(|i| {
+                let pool = pool.clone();
+                thread::spawn(move || {
+                    let conn = pool.get().unwrap();
+                    conn.execute("INSERT INTO foo (bar) VALUES (?)", &[&i])
+                        .unwrap();
+                })
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .map(thread::JoinHandle::join)
+            .collect::<Result<_, _>>()
+            .unwrap()
+    }
 
 }
