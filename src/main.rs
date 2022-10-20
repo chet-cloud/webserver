d0336813d6df5ad09bd054fd0673b7f41688a778
chet <cchen@artisreit.com>
2022-10-19 22:34:22 -0500

    save

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,32 @@ pub mod pool;
 pub mod utils;
 pub mod http;
 
+use std::collections::HashMap;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use pool::Pool;
 use http::Request;
 use http::Response;
 
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = Pool::new(5);
+    let mut handler_map:HashMap<&str, Box<dyn FnMut(Request,TcpStream) -> bool>> = HashMap::new();
+
+    handler_map.insert("GET /cc", Box::new(|request:Request,stream: TcpStream|->bool {
+        return true
+    }));
+
+    handler_map.insert("GET /ff", Box::new(|request:Request,stream: TcpStream|->bool {
+        return true
+    }));
+    
+
     for stream in listener.incoming(){
         let stream = stream.unwrap();
         pool.execute(move ||{
-            handle_connection(stream);
+            handle_connection(stream,handler_map);
         })
     }
     println!("Shutting down.");
@@ -23,7 +36,9 @@ fn main() {
 
 
 
-fn handle_connection(stream: TcpStream) {
+
+
+fn handle_connection(stream: TcpStream,map:HashMap<&str, Box<dyn FnMut(Request,TcpStream) -> bool>>) {
     let request = Request::parse(&stream);
     println!("{:?}",request.unwrap());
     // request.is_get_then("/test",|response|{
@@ -32,6 +47,9 @@ fn handle_connection(stream: TcpStream) {
     // request.is_post_then("/user",|response|{
     //     response.send_json();
     // });
+    
+    map.get("GET /cc").unwrap()(request.unwrap(),stream);
+
     Response::response_file(stream,"hello.html");
 }
 
